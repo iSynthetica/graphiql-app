385d8455c54eabfcbccf26edfcc630d6fba612e9
iSynthetica <syntheticafreon@gmail.com>
2023-12-15 16:36:43 +0200

    feat: setup editor page

---
+++ src/app/editor/page.tsx
@@ -0,0 +1,11 @@
+const EditorPage = () => {
+  return (
+    <main>
+      <div className="min-h-[90vh] flex items-center justify-center bg-green-custom-800 text-white">
+        <h1>Editor Page</h1>
+      </div>
+    </main>
+  );
+};
+
+export default EditorPage;
