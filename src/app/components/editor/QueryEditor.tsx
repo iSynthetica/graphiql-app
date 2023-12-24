840d39d3a333828ee58504b781db174f3d29428a
IvanUpdate <mironovvan99@gmail.com>
2023-12-25 01:14:35 +0300

    add shortcut for prettier

--- src/app/components/editor/QueryEditor.tsx
+++ src/app/components/editor/QueryEditor.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/utils/cn';
 import styles from './editor.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { editor } from 'monaco-editor/esm/vs/editor/editor.api';
 import { prettifyGraphQLQuery } from '@/utils/prettier';
 import { changeQueryContent } from '@/redux/editorSlice';
@@ -58,6 +58,21 @@ const QueryEditor = () => {
     }
     //console.log(editorRef.current?.getValue());
   };
+
+
+  //add keyboard shortcut for prettier
+  useEffect(() => {
+    const handleGlobalKeyDown = (event: KeyboardEvent) => {
+      if (event.shiftKey && event.ctrlKey && event.key === 'P') {
+        event.preventDefault();
+        runPrettier();
+      }
+    };
+    window.addEventListener('keydown', handleGlobalKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleGlobalKeyDown);
+    };
+  }, []);
 
   return (
     <>
