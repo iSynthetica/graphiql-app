ae8b691b3be0f50a2724dbe537b641892152c746
Vladyslav Lisnyi <syntheticafreon@gmail.com>
2024-01-07 21:37:58 +0200

    feat: translate some strings

--- src/app/components/editor/EditorTabs.tsx
+++ src/app/components/editor/EditorTabs.tsx
@@ -3,21 +3,26 @@ import styles from './editor.module.scss';
 import { cn } from '@/utils/cn';
 import { setTab } from '@/redux/controlSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hook';
+import { ILocalizationContext } from '@/context/localization';
+import { useContext } from 'react';
 
 function EditorTabs() {
+  const { language, localization } = useContext(ILocalizationContext);
   const { tab } = useAppSelector((state: RootState) => state.control);
   const dispatch = useAppDispatch();
   return (
     <nav className={styles.editorTabNav}>
       <ul>
         <li className={cn(tab === 'headers' ? styles.editorTabNavActive : '')}>
-          <button onClick={() => dispatch(setTab('headers'))}>Headers</button>
+          <button onClick={() => dispatch(setTab('headers'))}>
+            {localization[language].headersTab}
+          </button>
         </li>
         <li
           className={cn(tab === 'variables' ? styles.editorTabNavActive : '')}
         >
           <button onClick={() => dispatch(setTab('variables'))}>
-            Variables
+            {localization[language].variablesTab}
           </button>
         </li>
       </ul>
